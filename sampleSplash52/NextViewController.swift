69e3733d2a392f43ca321227cce2d5f47ce58458
Reo O <rokumura@nexseed.net>
2019-04-25 13:51:05 +0800

    create project

---
+++ sampleSplash52/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  sampleSplash52
+//
+//  Created by reo on 2019/04/25.
+//  Copyright © 2019 ReoOkumura. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
